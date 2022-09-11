6871b82c919b2710528b5bb25833ec74d085831b
Shin <zhangzeshenyilang@gmail.com>
2022-09-11 16:53:38 +0900

    ログアウト時のroute変更

--- routes/artist.php
+++ routes/artist.php
@@ -24,9 +24,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('artist.welcome');
-});
+// Route::get('/', function () {
+//     return view('artist.welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('artist.dashboard');
